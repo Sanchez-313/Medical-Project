8a526b606c817277bf3c7c03b34346ff0b658765
Sanchez <minthantzin1929@gmail.com>
2026-02-18 08:51:14 +0630

    WIP: saving changes before rebase

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <div>
-        <h1 className="w-full text-center bg-green-500 text-3xl font-bold">This is Footer</h1>
-      </div>
-    </>
-  );
-}
